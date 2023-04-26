85bbeef62cb4f79623bef18a0bca6c14ec02dab2
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-04-26 11:08:03 +0300

    Добавил дз по поиску индекса массива, метод возвращает какой либо индекс первого найденного элемента, в противном случае выводится -1

---
+++ FindingIndex/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+
+/*  
+ *  Написать метод для поиска индекса массива (тип элемента в массиве - int),
+ *  метод долэен вернуть индекс первого найденного элемента (если он будет найден).
+ */
+
+namespace CharStringInMethods
+{
+    class Program
+    {
+        static int IndexOf(int [] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int[] GetRandomArray(uint length, int minValue, int maxValue)
+        {
+            int[] myArray = new int[length];
+
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = random.Next(minValue, maxValue);
+            }
+            return myArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = GetRandomArray ( 10, -20, 10 );
+
+            int result = IndexOf(myArray, 2);
+
+            WriteLine(result);
+        }
+    }
+}
