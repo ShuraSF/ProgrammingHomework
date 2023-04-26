580c8a36e32cf3312ba8e08956530be2e0f32174
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-04-26 10:37:04 +0300

    Добавьте файлы проекта.

---
+++ CharStringInMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+/*  
+ *  Написать метод который выводит на экран строку.
+ *  Символы из которых состоит строка и их количество вводится пользователем.
+ */
+
+namespace CharStringInMethods
+{
+    class Program
+    {
+        static void PrintLine(string symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Write(symbol);
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Введите символ : ");
+            string symbol = ReadLine();
+
+            WriteLine("Введите количество символов:");
+            uint symbolsCount = uint.Parse(ReadLine());
+
+            PrintLine($"{symbol} \t", symbolsCount);
+            ReadLine();
+        }
+    }
+}
