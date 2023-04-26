9970e777746eb59845a95801fd62b924c9bbf2ee
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-04-26 10:44:00 +0300

    Добавил вторую версию вывода метода но через тип Char

---
+++ CharStringInMethods_WithRestrictionInOneSymbol/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+/*  
+ *  Написать метод который выводит на экран строку.
+ *  Символы из которых состоит строка и их количество вводится пользователем.
+ */
+
+namespace CharStringInMethods
+{
+    class Program
+    {
+        static void PrintLine(char symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Write(symbol);
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Введите символ : ");
+            char symbol = ReadKey().KeyChar;
+
+            WriteLine("\nВведите количество символов:");
+            uint symbolsCount = uint.Parse(ReadLine());
+
+            PrintLine(symbol, symbolsCount);
+            ReadLine();
+        }
+    }
+}
